17fcbed1eb50744c42b775b70e454a05505f56e0
lnsjhnssn <144560735+lnsjhnssn@users.noreply.github.com>
2025-03-01 13:12:32 +0100

    changed how heroku gets stripe-key

--- app/javascript/src/property/bookingWidget.jsx
+++ app/javascript/src/property/bookingWidget.jsx
@@ -50,24 +50,18 @@ class BookingWidget extends React.Component {
     )
       .then(handleErrors)
       .then((response) => {
-        const stripe = Stripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
+        // Get Stripe key from meta tag
+        const stripeKey = document
+          .querySelector('meta[name="stripe-key"]')
+          .getAttribute("content");
+        const stripe = Stripe(stripeKey);
 
-        stripe
-          .redirectToCheckout({
-            // Make the id field from the Checkout Session creation API response
-            // available to this file, so you can provide it as parameter here
-            // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-            sessionId: response.charge.checkout_session_id,
-          })
-          .then((result) => {
-            // If `redirectToCheckout` fails due to a browser or network
-            // error, display the localized error message to your customer
-            // using `result.error.message`.
-          });
+        return stripe.redirectToCheckout({
+          sessionId: response.charge.checkout_session_id,
+        });
       })
-
       .catch((error) => {
-        console.log(error);
+        console.error("Payment error:", error);
       });
   };
 
